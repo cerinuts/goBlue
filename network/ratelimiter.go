44c72dd0a32e0880a6eaea10ff1715693dd86d59
ceriath <ceriath@ceriath.net>
2018-02-09 18:07:15 +0100

    doc

--- network/ratelimiter.go
+++ network/ratelimiter.go
@@ -1,3 +1,10 @@
+/*
+Copyright (c) 2017 ceriath
+This Package is part of the "goBlue"-Library
+It is licensed under the MIT License
+*/
+
+//Package network offers various network tools
 package network
 
 import (
